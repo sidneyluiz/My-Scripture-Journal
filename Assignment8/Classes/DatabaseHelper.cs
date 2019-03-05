7ebfac6f70ea1af21f23ee2e9cc669e3bcb8392d
Sidney Rodrigues <32102137+sidneyluiz@users.noreply.github.com>
2019-03-04 21:08:04 -0700

    Assigment Complete 8

---
+++ Assignment8/Classes/DatabaseHelper.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using WebMatrix.Data;
+
+namespace MyScriptureJournal.Classes
+{
+    class DatabaseHelper
+    {
+        public static object GetOne(string command, params object[] @params)
+        {
+            var db = OpenNewConnection();
+            Log(command, @params);
+
+            try
+            {
+                var data = db.QuerySingle(command, @params);
+                return data;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Close();
+            }
+        }
+
+        public static object Get(string command, params object[] @params)
+        {
+            var db = OpenNewConnection();
+            Log(command, @params);
+
+            try
+            {
+                var data = db.Query(command, @params);
+                return data;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Close();
+            }
+        }
+
+        public static bool Execute(string command, params object[] @params)
+        {
+            var db = OpenNewConnection();
+            Log(command, @params);
+
+            try
+            {
+                Debug.WriteLine($"---------------------------------------");
+                Debug.WriteLine($"SQL Command: {command}");
+                Debug.WriteLine($"Params: {@params}");
+                Debug.WriteLine($"---------------------------------------");
+
+                var result = db.Execute(command, @params);
+                return result > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Close();
+            }
+        }
+
+        static Database OpenNewConnection()
+        {
+            return Database.Open(nameof(MyScriptureJournal));
+        }
+
+        static void Log(string command, params object[] @params)
+        {
+            Debug.WriteLine($"---------------------------------------");
+            Debug.WriteLine($"SQL Command: {command}");
+
+            if (@params != null && @params.Any(wh => !string.IsNullOrEmpty(wh?.ToString())))
+                Debug.WriteLine($"Params: {string.Join(";", @params)}");
+
+            Debug.WriteLine($"---------------------------------------");
+        }
+    }
+}
---
+++ Assignment8/Classes/ScripturesDataHelper.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace MyScriptureJournal.Classes
+{
+    public class ScripturesDataHelper
+    {
+        const string Scriptures = "Scriptures";
+
+        public static dynamic GetByID(string id)
+        {
+            var command = $@"SELECT * FROM {Scriptures}
+                             WHERE ID = @0";
+
+            var data = DatabaseHelper.GetOne(command, id);
+            return data;
+        }
+
+        public static bool Delete(string id)
+        {
+            var command = $@"DELETE FROM {Scriptures} WHERE Id=@0";
+
+            return DatabaseHelper.Execute(command, id);
+        }
+
+        public static bool Update(string id, string book = null, int chapter = 0, int verse = 0, string note = null)
+        {
+            var command = $@"UPDATE {Scriptures} 
+                                SET Book=@1, Chapter=@2, Verse=@3, Note=@4
+                                WHERE Id=@0";
+
+            return DatabaseHelper.Execute(command, id, book, chapter, verse, note);
+        }
+
+        public static dynamic Search(string searchBook = null, string searchKeyword = null)
+        {
+            var command = "SELECT * FROM Scriptures WHERE Id <> 0 ";
+
+            if (!string.IsNullOrEmpty(searchBook))
+            {
+                command += " AND Book LIKE @0 ";
+                searchBook = $"%{searchBook}%";
+            }
+
+            if (!string.IsNullOrEmpty(searchKeyword))
+            {
+                command += @" AND Note LIKE @1 ";
+                searchKeyword = $"%{searchKeyword}%";
+            }
+
+            return DatabaseHelper.Get(command, searchBook, searchKeyword);
+        }
+
+        public static bool Insert(string book, int chapter, int verse, string note)
+        {
+            var insertCommand = $@"INSERT INTO {Scriptures}
+                                    (Book, Chapter, Verse, Note, Date)
+                                VALUES(@0, @1, @2, @3, @4)";
+
+            return DatabaseHelper.Execute(insertCommand, book, chapter, verse, note, DateTime.Now);
+        }
+    }
+}
